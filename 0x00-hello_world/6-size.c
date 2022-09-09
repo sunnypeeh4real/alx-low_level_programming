1b2f84bf20f3173b4b81b5716b4858af9a321af3
sunnypeeh4real <okaforsundaynn@gmail.com>
2022-09-08 18:58:25 -0700

    task6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+include <stdio.h>
+/**
+* main - entry point
+* 
+* Return: Always 0 (success)
+*/
+int main (void)
+{
+	int w;
+	long int x;
+	long long int y;
+	char z;
+	float f;
+	printf("size of a char: %d bytes(s)\n" ; size of (z)) ;
+	printf("size of an int: %d bytes(s)\n" ; size of (w)) ;
+	printf("size of a long int:%d bytes(s)\n" ; size of (x)) ;
+	printf("size of a long long int:%d bytes(s)\n" ; size of (y)) ;
+	printf("size of a float int:%d bytes(s)\n"; size of(f));
+	return (0)
+}
