e212583ecdd7f9e200890c4192966a9ba2434bfc
sunnypeeh4real <okaforsundaynn@gmail.com>
2022-09-14 05:17:51 -0700

    Hot fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,18 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
+
 /**
-* main - print if the number is postive, zero, or negative
-*
-* Description: using the main function
-* this program prints "Programming is positive, zero, or negative
-* Return: 0
-*/
+ * main - Prints a text according number
+ *
+ * Return: Always (Success)
+ */
 int main(void)
 {
-char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
-{
-	putchar(ch);
+	int n, lastd;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastd = n % 10;
+
+	if (lastd > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
+	}
+	else if (lastd == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, lastd);
+	}
+	else if (lastd < 6 && lastd != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
+	}
+
+	return (0);
 }
-putchar('\n');
-return (0);
-}
+
+
--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,43 +1,21 @@
-nclude <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+
 /**
-* main - print if the number is postive, zero, or negative
-
-* Description: using the main function
-* this program prints "Programming is positive, zero, or negative
-* Return: 0
-*/
-* main - print if the number is postive, zero, or negative
-*
-* Description: using the main function
-* this program prints "Programming is positive, zero, or negative
-* Return: 0
-*/
+  * main - Prints the numbers since 0 to 9
+  *
+  * Return: Always (Success)
+  */
 int main(void)
-{int n;
-int l;
-
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-l = n % 10;
-
-if (l>5)
-char c;
-for (c='z'; c>='a'; c--)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, l);
-}
-else if (l == 0)
-{
-printf("Last digit of %d is %d and is 0\n", n, l);
-}
-else
-{
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-putchar(c);
-}
-putchar('\n');
-return (0);
+	char n;
 
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
 }
+
