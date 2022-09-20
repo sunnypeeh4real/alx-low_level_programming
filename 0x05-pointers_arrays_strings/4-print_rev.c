2cd83435db78a7ca2cc94ff0ea065b97e3110b8f
sunnypeeh4real <okaforsundaynn@gmail.com>
2022-09-19 17:33:38 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,9 +1,11 @@
-#include "main.h
+#include "main.h"
+
 /**
  * print_rev - prints a string in reverse
  * @s: the used string reference pointer
  * Return: 0
  */
+
 void print_rev(char *s)
 {
 	int i = 0;
