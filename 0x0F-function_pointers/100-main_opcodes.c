560d105ced02c9f3a3b8d8656e93f876034f7536
sunnypeeh4real <okaforsundaynn@gmail.com>
2022-10-12 04:46:25 -0700

    Updated Task

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,49 +1,42 @@
-include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * print_opcodes - print the opcodes of this program
- * @a: address of the main function
- * @n: number of bytes to print
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: void
+ * Return: Always 0 (Success)
  */
-void print_opcodes(char *a, int n)
+int main(int argc, char *argv[])
 {
-int i;
+	int bytes, i;
+	char *arr;
 
-for (i = 0; i < n; i++)
-{
-printf("%.2hhx", a[i]);
-if (i < n - 1)
-printf(" ");
-}
-printf("\n");
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
 
-}
+	bytes = atoi(argv[1]);
 
-/**
- * main - prints the opcodes of its own main function
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: O
- */
-int main(int argc, char **argv)
-{
-int a;
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 
-if (argc != 2)
-{
-printf("Error\n");
-exit(1);
-}
-a = atoi(argv[1]);
-if (a < 0)
-{
-printf("Error\n");
-exit(2);
-}
-print_opcodes((char *)&main, a);
-return (0);
+	arr = (char *)main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
+	return (0);
 }
